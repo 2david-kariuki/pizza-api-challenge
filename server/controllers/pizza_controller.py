22d789d575ed3c36d3e1226bdb6d94e6db18cd77
David <david48kariuki@gmail.com>
2025-06-14 02:10:28 +0300

    Add GET /pizzas and POST /restaurant_pizzas routes with validation

--- server/controllers/pizza_controller.py
+++ server/controllers/pizza_controller.py
@@ -1,7 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, jsonify
+from server.models.pizza import Pizza
 
-pizza_bp = Blueprint('pizzas', __name__)
+pizza_bp = Blueprint("pizzas", __name__)
 
-@pizza_bp.route('/pizzas')
-def index():
-    return { "message": "Pizza route is working!" }
+@pizza_bp.route("/pizzas", methods=["GET"])
+def get_pizzas():
+    pizzas = Pizza.query.all()
+    return jsonify([p.to_dict() for p in pizzas]), 200
--- server/controllers/restaurant_pizza_controller.py
+++ server/controllers/restaurant_pizza_controller.py
@@ -1,7 +1,27 @@
-from flask import Blueprint
+from flask import Blueprint, request, jsonify
+from server.models.restaurant_pizza import RestaurantPizza
+from server.models.restaurant import Restaurant
+from server.models.pizza import Pizza
+from server.app import db
 
-restaurant_pizza_bp = Blueprint('restaurant_pizzas', __name__)
+restaurant_pizza_bp = Blueprint("restaurant_pizzas", __name__)
 
-@restaurant_pizza_bp.route('/restaurant_pizzas')
-def index():
-    return { "message": "RestaurantPizza route is working!" }
+@restaurant_pizza_bp.route("/restaurant_pizzas", methods=["POST"])
+def create_restaurant_pizza():
+    data = request.get_json()
+
+    try:
+        price = data["price"]
+        pizza_id = data["pizza_id"]
+        restaurant_id = data["restaurant_id"]
+
+        rp = RestaurantPizza(price=price, pizza_id=pizza_id, restaurant_id=restaurant_id)
+        db.session.add(rp)
+        db.session.commit()
+
+        return jsonify(rp.to_dict()), 201
+
+    except KeyError:
+        return jsonify({"errors": ["Missing required fields"]}), 400
+    except ValueError as e:
+        return jsonify({"errors": [str(e)]}), 400
